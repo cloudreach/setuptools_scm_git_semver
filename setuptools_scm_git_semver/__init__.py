bcd8cb6014ca645d0cb726199e6f8904999e656d
Simon Eilting <simon.eilting@cloudreach.com>
2021-11-30 18:26:49 +0000

    feat: Compatibility with setuptools_scm 6.3
    BREAKING CHANGE: Everything

--- setuptools_scm_git_semver/__init__.py
+++ setuptools_scm_git_semver/__init__.py
@@ -1,46 +1,106 @@
-from datetime import datetime
-import subprocess
+import re
+from typing import NamedTuple, Optional, Sequence, Tuple
 
-import dateutil.parser
-import semver
-from setuptools_scm import get_version as setuptools_scm_version
+import setuptools_scm.version
 
-from .git import parse
+RC_RE = re.compile(r"rc.(?P<number>\d+)")
 
 
-COMMIT_TIME_ARGS = ['git', 'show', '--pretty=%cI', '-s']
-TIME_FORMAT = '%Y%m%dT%H%M%SZ'
+class VersionCore(NamedTuple):
+    major: int
+    minor: int
+    patch: int
+
+    def set(
+        self, major: int = None, minor: int = None, patch: int = None
+    ) -> "VersionCore":
+        return VersionCore(
+            self.major if major is None else major,
+            self.minor if minor is None else minor,
+            self.patch if patch is None else patch,
+        )
+
+    @classmethod
+    def from_str(cls, version) -> "VersionCore":
+        return cls(*(int(x) for x in version.split(".")))
+
+    def __str__(self) -> str:
+        return f"{self.major}.{self.minor}.{self.patch}"
 
 
-def get_version(**kwargs):
-    return setuptools_scm_version(parse=parse,
-                                  version_scheme='semver',
-                                  local_scheme='semver',
-                                  **kwargs)
+class _Semver(NamedTuple):
+    version_core: VersionCore
+    pre_release: Sequence[str]
+    build: str
+
+    def set(
+        self,
+        version_core: VersionCore = None,
+        pre_release: Sequence[str] = None,
+        build: str = None,
+    ) -> "_Semver":
+        return _Semver(
+            self.version_core if version_core is None else version_core,
+            self.pre_release if pre_release is None else pre_release,
+            self.build if build is None else build,
+        )
+
+    def get_rc(self) -> Tuple["_Semver", Optional[int]]:
+        for index, pr in enumerate(self.pre_release):
+            match = RC_RE.match(pr)
+            if match:
+                rc = int(match.group("number"))
+                head, (_, *tail) = self.pre_release[:index], self.pre_release[index:]
+                return (
+                    _Semver(
+                        self.version_core,
+                        (*head, *tail),
+                        self.build,
+                    ),
+                    rc,
+                )
+        return self, None
+
+    @classmethod
+    def from_tag(cls, tag: str) -> "_Semver":
+        public, _, build = tag.partition("+")
+        core, _, pre = public.partition("-")
+        return cls(VersionCore.from_str(core), pre.split("-") if pre else (), build)
+
+    def __str__(self) -> str:
+        parts = [str(self.version_core)]
+        if self.pre_release:
+            parts += f"-{'-'.join(self.pre_release)}"
+        if self.build:
+            parts += f"+{self.build}"
+        return "".join(parts)
 
 
-def version_scheme_semver(version):
-    tag = version.tag
+def guess_next_rc(version: setuptools_scm.version.ScmVersion) -> str:
+    """Next RC unless we are on an exact version."""
+    if version.exact and not version.dirty:
+        return str(version.format_with("{tag}"))
+    semver, rc = _Semver.from_tag(str(version.tag)).get_rc()
+    if rc is None:
+        return str(
+            semver.set(
+                version_core=semver.version_core.set(
+                    patch=semver.version_core.patch + 1
+                ),
+                pre_release=(*semver.pre_release, "rc.0"),
+            )
+        )
+    return str(semver.set(pre_release=(*semver.pre_release, f"rc.{rc + 1}")))
 
-    if not version.distance and not version.dirty:
-        return str(tag)
 
-    version.time = datetime.now() if version.dirty else _get_commit_time()
-    base = f'{tag}.' if tag.prerelease else f'{semver.bump_patch(str(tag))}-'
-    return version.format_with('{base}{time:{time_format}}', base=base, time_format=TIME_FORMAT)
-
-
-def local_scheme_semver(version):
-    if version.distance and version.node:
-        return f'+{version.node[1:]}'
-    return ''
-
-
-def _get_commit_time():
-    try:
-        commit_time = subprocess.check_output(COMMIT_TIME_ARGS, stderr=subprocess.DEVNULL).strip()
-        return dateutil.parser.isoparse(commit_time)
-    except Exception:
-        # This could happen, e.g., if there are no commits in the repo. We assume other git issues
-        # would break before this point and assume that case for the sake of simple error handling.
-        return datetime.now()
+def local_time_dot_node(version: setuptools_scm.version.ScmVersion) -> str:
+    """The datetime if dirty; the datetime.node if not on a tag."""
+    # the t and z are lowercase on purpose: Setuptools warns if they are not
+    time_format = "%Y%m%dt%H%M%Sz"
+    if version.exact or version.node is None:
+        return str(
+            version.format_choice("", "+d{time:{time_format}}", time_format=time_format)
+        )
+    return str(
+        version.format_with("+{time:{time_format}}.{node}", time_format=time_format)
+    )
